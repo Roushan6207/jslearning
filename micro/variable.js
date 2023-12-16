8ca9593ab38f723d1d9b38abae7ec83cdddc1261
Roushan6207 <143305280+Roushan6207@users.noreply.github.com>
2023-12-16 08:54:23 +0000

    config new file

---
+++ micro/variable.js
@@ -0,0 +1,21 @@
+const accountId =144553
+ let accountEmail ="roushanrambo@gamil.com"
+ var accountPassword ="12345"
+ accountCity ="bihar"
+
+//  accountId=2 // not allowed
+
+// if (condition) {
+    
+// }
+/*
+prefer not to use of var becaues of issue in block 
+
+
+*/
+let accountState;
+accountEmail="idfc.com"
+accountPassword="172"
+accountCity="nawada"
+  console.log(accountId)
+  console.table([accountId, accountEmail, accountPassword,accountCity,accountState])
