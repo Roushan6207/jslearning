924ad1b39c33e02c091e346c5e46b00384b05c8a
Roushan6207 <143305280+Roushan6207@users.noreply.github.com>
2023-12-17 08:02:27 +0000

    conversion

---
+++ micro/conversion.js
@@ -0,0 +1,12 @@
+// let score=33
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+//
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
