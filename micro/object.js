02d1045873374e06b759380e55bda0668cad476c
Roushan6207 <143305280+Roushan6207@users.noreply.github.com>
2024-01-12 06:17:08 +0000

    somethingremain

--- micro/object.js
+++ micro/object.js
@@ -41,5 +41,49 @@ jsuser.email="guli@gamil.com"
 
 //&&&&&&&&&& new restart &&&&&&&&&&&&&
 
+// const tinderuser=new object()
+const tinderuser={}
+
+tinderuser.id="2345993iq"
+
+
+// console.log(tinderuser);
+
+  const regularuser={
+    email:"roushanrambo@gamil.com",
+    fullname:{
+        username:{
+            firstname:"roushan",
+            lastname:"singh"
+        }
+
+    }
+  }
+//   console.log(regularuser.fullname.username.lastname);
+
+
+const obj1={1:"a",2:"b"}
+const obj2={3:"a",4:"b"}
+const obj3={5:"a",6:"b"}
+//
+
+const obj4={...obj1,...obj2}
+// console.log(obj4)
+
+// console.log(Object.keys(tinderuser));
+// console.log(Object.values(tinderuser));
+// console.log(Object.entries(tinderuser));
+
+console.log(tinderuser.hasOwnProperty('isloggedin'));
+
+
+
+
+
+
+
+
+
+
 
 
