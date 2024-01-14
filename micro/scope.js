8765933facabd63e9cf2eb2cf976d416daddba93
Roushan6207 <143305280+Roushan6207@users.noreply.github.com>
2024-01-14 11:32:47 +0000

    scope

---
+++ micro/scope.js
@@ -0,0 +1,38 @@
+//var c=811;
+let a=10;
+if(true){
+    let a=90;
+    const b=76;
+    var c=10;
+}
+//console.log(a)
+//console.log(b)
+//console.log(c)
+
+// $$$$ second lecture (((((((((())))))))))
+
+
+
+function one(){
+  const username="roushan";
+  function two(){
+    const website="aahuga"
+    console.log(username);
+  }
+ // console.log(website)
+  //two()
+}
+//one()
+
+
+
+if(true){
+    const username="gulli"
+    if(true){
+        const web="facebook"
+        //console.log(username+web);
+    }
+    
+}
+// %%%%%%%%  interesting ^^^^^^^^^^^^^
+
