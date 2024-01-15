6fca07849a79043dc625004bf5507f9f114eb9ef
Roushan6207 <143305280+Roushan6207@users.noreply.github.com>
2024-01-15 07:24:27 +0000

    arrow

---
+++ micro/arrow.js
@@ -0,0 +1,32 @@
+const user={
+    username:"roushan",
+    price: 100,
+    welcomemessage:function(){
+    console.log(`${this.username} ,welcome to website`);
+    console.log(this);
+    }
+}
+
+// user.welcomemessage()
+// user.username="sam"
+// user.welcomemessage()
+//console.log(this);
+
+const chai=() => {
+    let username="higr"
+    console.log(this)
+}
+//chai()
+
+const addtwo=(numb1,numb2) =>  (numb1+numb2)
+
+//console.log(addtwo(2,1))
+
+// ######## second leture  ^^^^^^^^^ 
+ // immediately invoked function expression (IIFE)
+
+ 
+
+
+
+
