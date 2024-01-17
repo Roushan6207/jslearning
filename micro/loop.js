2a1f27ca125562f7bfb9da38cd2fff4f7179b8fa
Roushan6207 <143305280+Roushan6207@users.noreply.github.com>
2024-01-17 07:04:06 +0000

    logical&controlcomplete

---
+++ micro/loop.js
@@ -0,0 +1,93 @@
+//if(2=="2")
+   // console.log("statement is true"),console.log("bsdk");
+
+    //  switch (key) {
+    //     case value:
+            
+    //         break;
+     
+    //     default:
+    //         break;
+    //  }
+
+    // const month=6;
+    // switch (month) {
+    //     case 1:
+    //         console.log("jan");
+            
+    //         break;
+    //     case 2:
+    //         console.log("feb");
+            
+    //         break;
+    //     case 3:
+    //         console.log("march");
+            
+    //         break;
+    //     case 4:
+    //         console.log("april");
+            
+        //     break;
+        // case 4:
+        //     console.log("april");
+            
+        //     break;
+            
+
+    
+        // default:
+        //     console.log("default is printed")
+        //     break;
+        //}
+const userEmail=[]
+if(userEmail){
+    //console.log("got email");
+}else{
+    //console.log("dont get email")
+}
+
+// falsy values
+//false,0,-0,BigInt 0n,"",null,undefined,NaN
+
+// truthy values
+// "0",'false'," ",[],{},function(){}
+
+// if(userEmail.length==0)
+//console.log("array is empty");
+
+const emptyObj={}
+//if(Object.keys(emptyObj).length===0)
+// console.log("object is empty")
+
+ // NUllish coalescing operator (??)  : null .umdefined
+
+ let val1;
+ val1=5 ?? 10;
+val1=null?? 12;
+ val1=undefined?? 77;
+  val1=1?? 2?? 3;
+
+// console.log(val1);
+
+
+// terniary operator
+// condition ? true:false
+
+const bookprice=100
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
