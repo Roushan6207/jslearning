b68c00c18ec966266fe09e91e460eaa60772191f
Roushan6207 <143305280+Roushan6207@users.noreply.github.com>
2024-01-17 11:02:24 +0000

    loopstart

---
+++ iteration/01.js
@@ -0,0 +1,16 @@
+//for
+
+for(let i=0;i<10;i++){
+    const  element=i;
+   // console.log(element);
+}
+
+for (let i = 0; i < 10; i++) {
+   // console.log(`outerloop value:${i}`);
+    for (let j = 0; j < 10; j++) {
+        //console.log(i+'*' + j + '='  +i*j);
+        
+    }
+    
+}
+
