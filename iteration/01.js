d49d89ee9c750bb6bdf7ad2a44a38763f78f2b2c
Roushan6207 <143305280+Roushan6207@users.noreply.github.com>
2024-01-18 10:16:26 +0000

    forof

--- iteration/01.js
+++ iteration/01.js
@@ -14,3 +14,48 @@ for (let i = 0; i < 10; i++) {
     
 }
 
+
+// for of
+const arr=[1,2,3,4,5]
+
+
+for (const iterator of arr) {
+   // console.log(iterator);
+    
+}
+const greet="helloduniya"
+
+for (const i of greet) {
+   // console.log(`each char is ${i}`)
+    
+}
+
+// maps
+
+const map=new Map();
+map.set('IN',"INDIA")
+map.set('RUS',"RUSSIA")
+map.set('PAK',"PAKISTAN")
+map.set('IN',"INDIA")
+
+// console.log(map);
+// console.log(map.size);
+
+for (const [key,value] of map) {
+    console.log(key ,':->' ,value);
+    
+}
+
+const myObject={
+    game1:'pubg',
+    game2:'callofduty'
+}
+// for (const [key,value] of myObject) {
+//     //console.log(key ,':->' ,value);
+    
+// }
+
+
+
+
+
