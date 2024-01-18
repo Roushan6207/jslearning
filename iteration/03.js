d122c067d46a4d2a242dd4be0a0498e2c377fb39
Roushan6207 <143305280+Roushan6207@users.noreply.github.com>
2024-01-18 10:53:03 +0000

    foreach

---
+++ iteration/03.js
@@ -0,0 +1,55 @@
+const coding=["js","rb","py","java","cpp"]
+
+// coding.forEach(  function (item) {
+//     console.log(item);
+// })
+
+
+// coding.forEach( (don) =>  {
+//     console.log(don)
+// })
+
+
+// function print(jaan){
+//     console.log(jaan)
+// }
+
+// coding.forEach(print)
+
+
+// coding.forEach(  (item,index,arr) =>{
+//     console.log(item,index,arr);
+// })
+
+
+
+const myCoding=[
+    {
+        languageName:"javascript",
+        languagefileName:"js"
+
+    },
+    {
+        languageName:"python",
+        languagefileName:"py"
+
+    },
+    {
+        languageName:"c++",
+        languagefileName:"cpp"
+
+    },
+
+]
+
+myCoding.forEach(  (item)=>{
+    console.log(item.languagefileName);
+
+})
+
+
+
+
+
+
+
